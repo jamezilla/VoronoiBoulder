a95e8f85264f070ad8bafcf9c8d88262bcc4ae2e
James Hughes <james@virtualjames.com>
2016-03-01 00:21:27 -0600

    added rainbow hue mode, update interval

--- sketches/NoiseTestTwo/Nodule.cpp
+++ sketches/NoiseTestTwo/Nodule.cpp
@@ -1,34 +1,98 @@
 #include "Nodule.h"
 
+#define DEFAULT_UPDATE_INTERVAL 100
+#define DEFAULT_HUE_MODE        Perlin
+#define DEFAULT_SATURATION_MODE Constant
+
 Nodule::Nodule()
-  : leds(nullptr),
+  : hue(UINT8_MAX),
+    leds(nullptr),
+    hueMode(Rainbow),
     noiseSeed(0),
     noiseSpeedAccumulator(0),
-    noiseSpeedIncrement(0)
+    noiseSpeedIncrement(0),
+    previousTime(0),
+    saturation(UINT8_MAX),
+    saturationMode(DEFAULT_SATURATION_MODE),
+    updateInterval(DEFAULT_UPDATE_INTERVAL),
+    value(UINT8_MAX)
 {}
 
 Nodule::Nodule(CRGB *leds)
-  : leds(leds),
+  : hue(UINT8_MAX),
+    leds(leds),
+    hueMode(Rainbow),
     noiseSeed(random16()),
     noiseSpeedAccumulator(0),
-    noiseSpeedIncrement(MAX_SPEED * random16() / (float)UINT16_MAX)
+    noiseSpeedIncrement(MAX_SPEED * random16() / (float)UINT16_MAX),
+    previousTime(0),
+    saturation(UINT8_MAX),
+    saturationMode(DEFAULT_SATURATION_MODE),
+    updateInterval(DEFAULT_UPDATE_INTERVAL),
+    value(UINT8_MAX)
 {}
 
+void Nodule::setUpdateInterval(uint16_t updateInterval) {
+  this->updateInterval = updateInterval;
+}
+
+void Nodule::setHueMode(HueMode hueMode) {
+  this->hueMode = hueMode;
+}
+
+void Nodule::setSaturationMode(SaturationMode saturationMode) {
+  this->saturationMode = saturationMode;
+}
+
 void Nodule::update() {
-    uint8_t hue = inoise8(noiseSeed);
+  uint32_t now = millis();
 
-    for (uint8_t j = 0; j < NUM_LEDS_PER_NODE; j++) {
-      this->leds[j] = CHSV(hue, UINT8_MAX, UINT8_MAX);
-    }
+  // has enough time elapsed for us to update?
+  if ((now - previousTime) < this->updateInterval) {
+    return;
+  }
 
-    // accumulate time
-    this->noiseSpeedAccumulator += this->noiseSpeedIncrement;
+  // what hueMode are we in?
+  switch(this->hueMode) {
+  case Rainbow:
+    updateRainbow();
+    break;
 
-    // advance time - either 0 or 1 since we're casting a float of value: 0 < noiseSpeedAccumulator < 1
-    this->noiseSeed += (uint16_t) this->noiseSpeedAccumulator;
+  case Perlin:
+  default:
+    updatePerlin();
+    break;
 
-    // clamp to less than 1
-    if (this->noiseSpeedAccumulator > MAX_SPEED) {
-      this->noiseSpeedAccumulator = MAX_SPEED - this->noiseSpeedAccumulator;
-    }
+  }
+
+  // paint the leds
+  updateLeds();
+
+  // keep track of when we last updated
+  this->previousTime = now;
+}
+
+void Nodule::updateLeds() {
+  for (uint8_t j = 0; j < NUM_LEDS_PER_NODE; j++) {
+    this->leds[j] = CHSV(this->hue, this->saturation, this->value);
+  }
+}
+
+void Nodule::updatePerlin() {
+  this->hue = inoise8(noiseSeed);
+
+  // accumulate time
+  this->noiseSpeedAccumulator += this->noiseSpeedIncrement;
+
+  // advance time - either 0 or 1 since we're casting a float of value: 0 < noiseSpeedAccumulator < 1
+  this->noiseSeed += (uint16_t) this->noiseSpeedAccumulator;
+
+  // clamp to less than 1
+  if (this->noiseSpeedAccumulator > MAX_SPEED) {
+    this->noiseSpeedAccumulator = MAX_SPEED - this->noiseSpeedAccumulator;
+  }
+}
+
+void Nodule::updateRainbow() {
+  this->hue += 1;
 }
--- sketches/NoiseTestTwo/Nodule.h
+++ sketches/NoiseTestTwo/Nodule.h
@@ -4,13 +4,43 @@
 class Nodule {
 
 public:
+  typedef uint8_t Hue;
+  typedef uint8_t Saturation;
+  typedef uint8_t Value;
+
+  typedef enum HueMode {
+    Perlin,
+    Rainbow
+  } HueMode;
+
+  typedef enum SaturationMode {
+    Constant
+  } SaturationMode;
+
   Nodule();
   Nodule(CRGB *leds);
+
+  void setMode(Mode mode);
+  void setSaturationMode(SaturationMode saturationMode);
+  void setUpdateInterval(uint16_t updateInterval);
   void update();
 
 private:
-  CRGB     *leds;
-  uint16_t noiseSeed;
-  float    noiseSpeedAccumulator;
-  float    noiseSpeedIncrement;
+  void updateLeds();
+  void updatePerlin();
+  void updateRainbow();
+
+  CRGB       *leds;
+  HueMode    hueMode;
+  uint16_t   updateInterval;
+  uint32_t   previousTime;
+  Hue        hue;
+  Saturation saturation;
+  Value      value;
+
+  // Perlin noise
+  uint16_t   noiseSeed;
+  float      noiseSpeedAccumulator;
+  float      noiseSpeedIncrement;
+
 };
