52bef6e844c82b12547e0cf76c5e4bc304f2bcdb
James Hughes <james@virtualjames.com>
2016-03-01 01:37:14 -0600

    added controls for saturation and value

--- sketches/NoiseTestTwo/Nodule.cpp
+++ sketches/NoiseTestTwo/Nodule.cpp
@@ -1,82 +1,137 @@
 #include "Nodule.h"
 
-#define DEFAULT_UPDATE_INTERVAL 100
-#define DEFAULT_HUE_MODE        Perlin
-#define DEFAULT_SATURATION_MODE Constant
+#define DEFAULT_HUE_MODE                   HueModeRainbow
+#define DEFAULT_HUE_UPDATE_INTERVAL        100
+#define DEFAULT_SATURATION_MODE            SaturationModeConstant
+#define DEFAULT_SATURATION_UPDATE_INTERVAL 100
+#define DEFAULT_VALUE_MODE                 ValueModeConstant
+#define DEFAULT_VALUE_UPDATE_INTERVAL      100
 
 Nodule::Nodule()
   : hue(UINT8_MAX),
+    hueMode(DEFAULT_HUE_MODE),
+    hueUpdateInterval(DEFAULT_HUE_UPDATE_INTERVAL),
     leds(nullptr),
-    hueMode(Rainbow),
     noiseSeed(0),
     noiseSpeedAccumulator(0),
     noiseSpeedIncrement(0),
     previousTime(0),
     saturation(UINT8_MAX),
     saturationMode(DEFAULT_SATURATION_MODE),
-    updateInterval(DEFAULT_UPDATE_INTERVAL),
-    value(UINT8_MAX)
+    saturationUpdateInterval(DEFAULT_SATURATION_UPDATE_INTERVAL),
+    value(UINT8_MAX),
+    valueMode(DEFAULT_VALUE_MODE),
+    valueUpdateInterval(DEFAULT_VALUE_UPDATE_INTERVAL)
 {}
 
+
 Nodule::Nodule(CRGB *leds)
-  : hue(UINT8_MAX),
+  : hue(128),
+    hueMode(DEFAULT_HUE_MODE),
+    hueUpdateInterval(DEFAULT_HUE_UPDATE_INTERVAL),
     leds(leds),
-    hueMode(Rainbow),
     noiseSeed(random16()),
     noiseSpeedAccumulator(0),
     noiseSpeedIncrement(MAX_SPEED * random16() / (float)UINT16_MAX),
     previousTime(0),
     saturation(UINT8_MAX),
     saturationMode(DEFAULT_SATURATION_MODE),
-    updateInterval(DEFAULT_UPDATE_INTERVAL),
-    value(UINT8_MAX)
+    saturationUpdateInterval(DEFAULT_SATURATION_UPDATE_INTERVAL),
+    value(UINT8_MAX),
+    valueMode(DEFAULT_VALUE_MODE),
+    valueUpdateInterval(DEFAULT_VALUE_UPDATE_INTERVAL)
 {}
 
-void Nodule::setUpdateInterval(uint16_t updateInterval) {
-  this->updateInterval = updateInterval;
+
+void Nodule::setHue(Hue hue) {
+  this->hue = hue;
 }
 
-void Nodule::setHueMode(HueMode hueMode) {
-  this->hueMode = hueMode;
+
+void Nodule::setHueMode(HueMode mode) {
+  this->hueMode = mode;
 }
 
-void Nodule::setSaturationMode(SaturationMode saturationMode) {
-  this->saturationMode = saturationMode;
+
+void Nodule::setHueUpdateInterval(uint16_t interval) {
+  this->hueUpdateInterval = interval;
 }
+
+
+void Nodule::setSaturation(Saturation saturation) {
+  this->saturation = saturation;
+}
+
+
+void Nodule::setSaturationMode(SaturationMode mode) {
+  this->saturationMode = mode;
+}
+
+
+void Nodule::setSaturationUpdateInterval(uint16_t interval) {
+  this->saturationUpdateInterval = interval;
+}
+
+
+void Nodule::setValue(Value value) {
+  this->value = value;
+}
+
+
+void Nodule::setValueMode(ValueMode mode) {
+  this->valueMode = mode;
+}
+
+
+void Nodule::setValueUpdateInterval(uint16_t interval) {
+  this->valueUpdateInterval = interval;
+}
+
 
 void Nodule::update() {
   uint32_t now = millis();
+  uint32_t elapsed = now - previousTime;
 
   // has enough time elapsed for us to update?
-  if ((now - previousTime) < this->updateInterval) {
+  if (elapsed < this->hueUpdateInterval) {
     return;
   }
 
-  // what hueMode are we in?
-  switch(this->hueMode) {
-  case Rainbow:
-    updateRainbow();
-    break;
-
-  case Perlin:
-  default:
-    updatePerlin();
-    break;
-
-  }
-
-  // paint the leds
+  updateHue(elapsed);
+  updateSaturation(elapsed);
+  updateValue(elapsed);
   updateLeds();
 
   // keep track of when we last updated
   this->previousTime = now;
 }
 
+
+void Nodule::updateHue(uint32_t &elapsed) {
+  // has enough time elapsed for us to update?
+  if (elapsed < this->hueUpdateInterval) {
+    return;
+  }
+
+  // update the hue
+  switch(this->hueMode) {
+  case HueModeRainbow:
+    updateRainbow();
+    break;
+  case HueModePerlin:
+  default:
+    updatePerlin();
+    break;
+  }
+}
+
+
 void Nodule::updateLeds() {
   for (uint8_t j = 0; j < NUM_LEDS_PER_NODE; j++) {
     this->leds[j] = CHSV(this->hue, this->saturation, this->value);
   }
 }
+
 
 void Nodule::updatePerlin() {
   this->hue = inoise8(noiseSeed);
@@ -93,6 +148,39 @@ void Nodule::updatePerlin() {
   }
 }
 
+
 void Nodule::updateRainbow() {
   this->hue += 1;
+}
+
+
+void Nodule::updateSaturation(uint32_t &elapsed) {
+  // has enough time elapsed for us to update?
+  if (elapsed < this->saturationUpdateInterval) {
+    return;
+  }
+
+  // update the saturation
+  switch(this->saturationMode) {
+  case SaturationModeConstant:
+  default:
+    // noop
+    break;
+  }
+}
+
+
+void Nodule::updateValue(uint32_t &elapsed) {
+  // has enough time elapsed for us to update?
+  if (elapsed < this->valueUpdateInterval) {
+    return;
+  }
+
+  // update the value
+  switch(this->valueMode) {
+  case ValueModeConstant:
+  default:
+    // noop
+    break;
+  }
 }
--- sketches/NoiseTestTwo/Nodule.h
+++ sketches/NoiseTestTwo/Nodule.h
@@ -9,38 +9,61 @@ public:
   typedef uint8_t Value;
 
   typedef enum HueMode {
-    Perlin,
-    Rainbow
+    HueModePerlin,
+    HueModeRainbow
   } HueMode;
 
   typedef enum SaturationMode {
-    Constant
+    SaturationModeConstant
   } SaturationMode;
+
+  typedef enum ValueMode {
+    ValueModeConstant
+  } ValueMode;
 
   Nodule();
   Nodule(CRGB *leds);
 
-  void setMode(Mode mode);
-  void setSaturationMode(SaturationMode saturationMode);
-  void setUpdateInterval(uint16_t updateInterval);
+  void setHue(Hue hue);
+  void setHueUpdateInterval(uint16_t interval);
+  void setHueMode(HueMode mode);
+
+  void setSaturation(Saturation saturation);
+  void setSaturationUpdateInterval(uint16_t interval);
+  void setSaturationMode(SaturationMode mode);
+
+  void setValue(Value value);
+  void setValueUpdateInterval(uint16_t interval);
+  void setValueMode(ValueMode mode);
+
   void update();
 
 private:
+  void updateHue(uint32_t &elapsed);
   void updateLeds();
   void updatePerlin();
   void updateRainbow();
+  void updateSaturation(uint32_t &elapsed);
+  void updateValue(uint32_t &elapsed);
 
-  CRGB       *leds;
-  HueMode    hueMode;
-  uint16_t   updateInterval;
-  uint32_t   previousTime;
-  Hue        hue;
-  Saturation saturation;
-  Value      value;
+  CRGB           *leds;                     // our slice of the LED array
+  uint32_t       previousTime;              // the time of our last update
+
+  Hue            hue;
+  HueMode        hueMode;
+  uint16_t       hueUpdateInterval;
+
+  Saturation     saturation;
+  SaturationMode saturationMode;
+  uint16_t       saturationUpdateInterval;
+
+  Value          value;
+  ValueMode      valueMode;
+  uint16_t       valueUpdateInterval;
 
   // Perlin noise
-  uint16_t   noiseSeed;
-  float      noiseSpeedAccumulator;
-  float      noiseSpeedIncrement;
+  uint16_t       noiseSeed;                 // where do we start in the noise field?
+  float          noiseSpeedAccumulator;     // keep a rolling tally of how far we've gone
+  float          noiseSpeedIncrement;       // how much to add on each iteration
 
 };
